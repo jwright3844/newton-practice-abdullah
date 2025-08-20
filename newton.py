d3828b5303344ae919cdfef5d87dd06652151dd2
Abdullah Ateyeh <abdullah_ateyeh@berkeley.edu>
2025-08-20 13:19:29 -0700

    adding test cases

--- newton.py
+++ newton.py
@@ -13,3 +13,22 @@ def optimize(x_0, f, epsilon=0.0001, threshold=0.0001):
         prev_x = curr_x
         curr_x -= f_d(curr_x, f, epsilon)/f_dd(curr_x, f, epsilon)
     return(curr_x)
+
+def test_optimize():
+    def f_1(x):
+        return (x-3)**2
+    def f_2(x):
+        return np.sqrt(x**2 + 3)
+    def f_3(x):
+        return np.sin(x)
+
+    case_threshold = 0.001
+    print(f"Test Case 1:")
+    assert np.abs(optimize(1, f_1) - 3) < case_threshold
+    print(f"Success")
+    print(f"Test Case 2:")
+    assert np.abs(optimize(1, f_2) - 0) < case_threshold
+    print(f"Success")
+    print(f"Test Case 3:")
+    assert np.abs(optimize(3, f_3) - np.pi) < case_threshold
+    print(f"Success")

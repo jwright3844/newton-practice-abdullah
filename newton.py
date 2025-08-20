4c4d41aa4bda2602331a9176de5510cbe9ead42b
Abdullah Ateyeh <abdullah_ateyeh@berkeley.edu>
2025-08-20 13:38:26 -0700

    Fix formatting with ruff.

--- newton.py
+++ newton.py
@@ -1,22 +1,27 @@
 import numpy as np
 
+
 def f_d(x, f, epsilon):
-    return (f(x + epsilon) - f(x))/epsilon
+    return (f(x + epsilon) - f(x)) / epsilon
+
 
 def f_dd(x, f, epsilon):
-    return (f(x + epsilon) - 2*f(x) + f(x - epsilon))/(epsilon**2)
+    return (f(x + epsilon) - 2 * f(x) + f(x - epsilon)) / (epsilon**2)
+
 
 def optimize(x_0, f, epsilon=0.0001, threshold=0.0001):
     curr_x = x_0
     prev_x = np.inf
-    while np.abs(curr_x -prev_x) > threshold:
+    while np.abs(curr_x - prev_x) > threshold:
         prev_x = curr_x
-        curr_x -= f_d(curr_x, f, epsilon)/f_dd(curr_x, f, epsilon)
-    return(curr_x)
+        curr_x -= f_d(curr_x, f, epsilon) / f_dd(curr_x, f, epsilon)
+    return curr_x
+
 
 def test_optimize():
     def f_1(x):
-        return (x-3)**2
+        return (x - 3) ** 2
+
     def f_2(x):
         return np.sqrt(x**2 + 3)
 
